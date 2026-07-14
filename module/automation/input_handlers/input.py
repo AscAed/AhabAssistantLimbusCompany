603c50c8c270e6b48a0384c786f803228233064d
AscAed <116534369+AscAed@users.noreply.github.com>
2026-07-14 12:13:43 +0800

    Update input.py

--- module/automation/input_handlers/input.py
+++ module/automation/input_handlers/input.py
@@ -408,8 +408,8 @@ class BackgroundInput(WinAbstractInput, metaclass=SingletonMeta):
         msg = f"点击位置:({x},{y})"
         log.debug(msg, stacklevel=2)
         for i in range(times):
-            self._post_bezier_move(x, y)
             self.set_active()
+            self._post_bezier_move(x, y)
             self.mouse_down(x, y)
             sleep(humanised_delay(0.05, "gaussian"))  # 模拟人类按下和松开的时间间隔
             self.mouse_up(x, y)
@@ -532,8 +532,8 @@ class BackgroundInput(WinAbstractInput, metaclass=SingletonMeta):
         x = coordinate[0] + random.randint(0, 10)
         y = coordinate[1] + random.randint(0, 10)
         for i in range(times):
-            self._post_bezier_move(x, y)
             self.set_active()
+            self._post_bezier_move(x, y)
             self.mouse_down(x, y)
             sleep(humanised_delay(0.05, "gaussian"))  # 模拟人类按下 and 松开时间间隔
             self.mouse_up(x, y)
